d25bedaed3b6b9e83b0e73bcadbf3e6024df2510
edoardo <phate09@hotmail.it>
2019-03-15 15:43:32 +0000

    added examples fixed communication_client

--- protobuf/communication_client.py
+++ protobuf/communication_client.py
@@ -10,13 +10,15 @@ class CommunicationChannel:
     def __init__(self):
         self.context = zmq.Context()
         self.socket = self.context.socket(zmq.PUB)
-        self.socket.bind("tcp://*:%s" % port)
+        self.socket.bind("tcp://*:5556")
 
     def send(self, simulation_time, heatmap: np.ndarray):
+        # while True:
         heatmap_message: protobuf.heatmap_pb2.Heatmap = protobuf.heatmap_pb2.Heatmap()
         heatmap_message.time = simulation_time
         size = heatmap.shape
         size = list(size)
         heatmap_message.size[:] = size
         heatmap_message.map[:] = list(heatmap.flatten())
-        self.socket.send_string(heatmap_message.SerializeToString())
+        self.socket.send(heatmap_message.SerializeToString())
+        # self.socket.send_string("10001 hello")
